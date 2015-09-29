c6762076e4aa292639d5f316985b76e900245baa
Aharon h. <aharon.topq@gmail.com>
2015-09-29 20:46:10 +0300

    Panel pins are now given

--- src/main/java/pi/pushbutton/control/Panel.java
+++ src/main/java/pi/pushbutton/control/Panel.java
@@ -11,35 +11,29 @@ import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioProvider;
 import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.PinMode;
-import com.pi4j.io.gpio.impl.PinImpl;
 
 public class Panel extends ObserveableComponentBase {
 	protected PushButtonWithLED red, green, blue, yellow, white;
-	final Pin redPin, greenPin, bluePin, yellowPin, whitePin;
+	// final Pin redPin, greenPin, bluePin, yellowPin, whitePin;
 	protected final PushButtonWithLED[] allPushButtonWithLEDs;
 	protected final static EnumSet<PinMode> DIGITAL_IO_MODES = EnumSet.of(
 			PinMode.DIGITAL_INPUT, PinMode.DIGITAL_OUTPUT);
 
 	public Panel(GpioController gpio, GpioProvider providerForButtons,
-			GpioProvider providerForLEDs) {
-		String providerName = providerForButtons.getName();
-		redPin = new PinImpl(providerName, 0, "red", DIGITAL_IO_MODES);
-		greenPin = new PinImpl(providerName, 1, "green", DIGITAL_IO_MODES);
-		bluePin = new PinImpl(providerName, 2, "blue", DIGITAL_IO_MODES);
-		yellowPin = new PinImpl(providerName, 3, "yellow", DIGITAL_IO_MODES);
-		whitePin = new PinImpl(providerName, 4, "white", DIGITAL_IO_MODES);
-
-		red = new PushButtonWithLED(gpio, providerForButtons, redPin,
-				providerForLEDs, redPin);
-
-		green = new PushButtonWithLED(gpio, providerForButtons, greenPin,
-				providerForLEDs, greenPin);
-		blue = new PushButtonWithLED(gpio, providerForButtons, bluePin,
-				providerForLEDs, bluePin);
-		yellow = new PushButtonWithLED(gpio, providerForButtons, yellowPin,
-				providerForLEDs, yellowPin);
-		white = new PushButtonWithLED(gpio, providerForButtons, whitePin,
-				providerForLEDs, whitePin);
+			GpioProvider providerForLEDs, Pin redButtonPin, Pin greenButtonPin,
+			Pin blueButtonPin, Pin yellowButtonPin, Pin whiteButtonPin,
+			Pin redLEDPin, Pin greenLEDPin, Pin blueLEDPin, Pin yellowLEDPin,
+			Pin whiteLEDPin) {
+		red = new PushButtonWithLED(gpio, providerForButtons, redButtonPin,
+				providerForLEDs, redLEDPin, "RED");
+		green = new PushButtonWithLED(gpio, providerForButtons, greenButtonPin,
+				providerForLEDs, greenLEDPin, "GREEN");
+		blue = new PushButtonWithLED(gpio, providerForButtons, blueButtonPin,
+				providerForLEDs, blueLEDPin, "BLUE");
+		yellow = new PushButtonWithLED(gpio, providerForButtons,
+				yellowButtonPin, providerForLEDs, yellowLEDPin, "YELLOW");
+		white = new PushButtonWithLED(gpio, providerForButtons, whiteButtonPin,
+				providerForLEDs, whiteLEDPin, "WHITE");
 		allPushButtonWithLEDs = new PushButtonWithLED[] { red, green, blue,
 				yellow, white };
 		if (this instanceof SwitchListener) {
